895f9fb0fef273a2ce0adda3e1d340ea8f799d95
Victor Franco <vfrancohdez1@hotmail.com>
2020-09-03 19:11:42 -0500

    Refactor the Stack for readability
    Create a module and implement methods with the functions.
    
    As the code increase it's necessary to separate the functionality.
    
    Create stack.rs file.
    
    Use the fuctions as structure methods.

--- src/main.rs
+++ src/main.rs
@@ -1,49 +1,16 @@
-//N=array size
-const N:usize=10;
-struct Stack{
-    array:[f64;N],
-    sp:i32
-}
-fn create_stack()->Stack{
-    Stack{
-        array:[0.0;N],
-        sp:(N as i32)-1
-    }
-}
-fn push(stack:&mut Stack,value:f64)->bool{
-    if !is_full(stack){        
-        stack.array[stack.sp as usize]=value;
-        stack.sp=stack.sp-1;
-        return true
-    }
-    return false
-}
-fn pop(stack:&mut Stack,value:&mut f64)->bool{
-    if !is_empty(stack){
-        stack.sp=stack.sp+1;
-        *value=stack.array[stack.sp as usize];
-        stack.array[stack.sp as usize]=0.0;
-        return true
-    }
-    return false
-}
-fn is_full(stack:&mut Stack)->bool{
-    stack.sp==-1
-}
-fn is_empty(stack:&mut Stack)->bool{
-    stack.sp==(N as i32)-1
-}
+mod stack;
+use stack::Stack as Stack;
 fn main(){
     println!("Calculator");
-    let mut stack:Stack=create_stack();
+    let mut stack:Stack=Stack::create_stack();
     let instructions=["1","2","+"];
     let mut num1:f64=0.0;
     let mut num2:f64=0.0;    
     for instruction in instructions.iter(){    
         let expression=*instruction;
         if expression=="+"||expression=="-"||expression=="*"||expression=="/" {
-            pop(&mut stack,&mut num1);
-            pop(&mut stack,&mut num2);
+            Stack::pop(&mut stack,&mut num1);
+            Stack::pop(&mut stack,&mut num2);
             let result=match &*expression{
                 "+"=>num1+num2,
                 "-"=>num1-num2,
@@ -51,10 +18,10 @@ fn main(){
                 "/"=>num1/num2,
                 _=>0.0
             };
-            push(&mut stack,result);
+            Stack::push(&mut stack,result);
         }else{
             let number:f64=expression.parse::<f64>().unwrap();
-            push(&mut stack,number);
+            Stack::push(&mut stack,number);
         }
     }
     let mut math_expre=String::new();
---
+++ src/stack.rs
@@ -0,0 +1,38 @@
+//N=array size
+const N:usize=10;
+pub struct Stack{
+    pub array:[f64;N],
+    pub sp:i32
+}
+impl Stack{
+    pub fn create_stack()->Stack{
+        Stack{
+            array:[0.0;N],
+            sp:(N as i32)-1
+        }
+    }
+    pub fn push(&mut self,value:f64)->bool{
+        if !self.is_full(){        
+            self.array[self.sp as usize]=value;
+            self.sp=self.sp-1;
+            return true
+        }
+        return false
+    }
+    pub fn pop(&mut self,value:&mut f64)->bool{
+        if !self.is_empty(){
+            self.sp=self.sp+1;
+            *value=self.array[self.sp as usize];
+            self.array[self.sp as usize]=0.0;
+            return true
+        }
+        return false
+    }
+    pub fn is_full(&self)->bool{
+        self.sp==-1
+    }
+    pub fn is_empty(&self)->bool{
+        self.sp==(N as i32)-1
+    }
+    
+}
