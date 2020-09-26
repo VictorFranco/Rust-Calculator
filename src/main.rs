569da7b51057fd91f1d351f9f19638d0d7da1356
Victor Franco <vfrancohdez1@hotmail.com>
2020-09-26 07:35:02 -0500

    Create the Tree Data Structure
    Create a binary three and add the tree methods.
    
    That structure is used to transform the expression order. The
    notation, which we used, is called infix and the notation, which the
    stack needs to compute the expression, is called postfix.
    
    Create the tree module and implement the methods. When the tree has the
    data, you should use the method post_order() and use the method
    compute() which computes a posfix expression.

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,7 @@
 mod stack;
 mod calculator;
+mod tree;
+use tree::Tree as Tree;
 fn main(){
     println!("Calculator\n");
 
@@ -12,4 +14,16 @@ fn main(){
     let result=calculator::compute(&mut instructions); 
     println!(">>> {}",calculator::get_math_expre(& mut instructions));
     println!("{}",result);
+    
+    println!("\n");
+    let mut t:Tree<String>=Tree::create_tree();
+    Tree::insert_node(&mut t,String::from("60")); Tree::pre_order(&t); println!("");
+    Tree::insert_node(&mut t,String::from("70")); Tree::pre_order(&t); println!("");
+    Tree::insert_node(&mut t,String::from("65")); Tree::pre_order(&t); println!("");
+    Tree::insert_node(&mut t,String::from("40")); Tree::pre_order(&t); println!("");
+    Tree::insert_node(&mut t,String::from("50")); Tree::pre_order(&t); println!("");
+    println!("\n");
+    
+    Tree::in_order(&t); println!("");
+    Tree::post_order(&t);
 }
---
+++ src/tree.rs
@@ -0,0 +1,124 @@
+pub struct Node<String>{
+    pub left:Option<Box<Node<String>>>,
+    pub right:Option<Box<Node<String>>>,
+    pub value:String
+}
+pub struct Tree<String>{
+    pub root:Option<Box<Node<String>>>
+}
+impl<String: std::fmt::Debug + std::cmp::PartialOrd> Node<String>{
+    pub fn pre_order(&self){
+        print!("{:?}", self.value);
+        match &self.left{
+            Some(p)=>{
+                print!("(");
+                Node::pre_order(&p);
+            },
+            None=>{},
+        }
+        match &self.right{
+            Some(p)=>{
+                Node::pre_order(&p);
+                print!(")");
+            },
+            None=>{},
+        }
+    }
+    pub fn in_order(&self){
+        match &self.left{
+            Some(p)=>{
+                print!("(");
+                Node::pre_order(&p);
+            },
+            None=>{},
+        }
+        print!("{:?}", self.value);
+        match &self.right{
+            Some(p)=>{
+                Node::pre_order(&p);
+                print!(")");
+            },
+            None=>{},
+        }
+    }
+    pub fn post_order(&self){
+        match &self.left{
+            Some(p)=>{
+                print!("(");
+                Node::pre_order(&p);
+            },
+            None=>{},
+        }
+        match &self.right{
+            Some(p)=>{
+                Node::pre_order(&p);
+                print!(")");
+            },
+            None=>{},
+        }
+        print!("{:?}", self.value);
+    }
+    pub fn create_node(val:String)->Node<String>{
+        Node{
+            left: None,
+            right: None,
+            value: val
+        }
+    }
+    pub fn insert_node(&mut self,val:String){
+        if val<=self.value{
+            match &mut self.left{
+                Some(l)=>Node::insert_node(l,val),
+                None=>{
+                    let node=Node::create_node(val);
+                    let pointer=Some(Box::new(node));
+                    self.left=pointer;
+                }
+            }
+        }else{
+            match &mut self.right{
+                Some(r)=>Node::insert_node(r,val),
+                None=>{
+                    let node=Node::create_node(val);
+                    let pointer=Some(Box::new(node));
+                    self.right=pointer;
+                }
+            }
+        }
+    }
+}
+impl<String: std::fmt::Debug + std::cmp::PartialOrd> Tree<String>{
+    pub fn create_tree()->Tree<String>{
+        Tree{
+            root: None
+        }
+    }
+    pub fn pre_order(&self){
+        match &self.root{
+            Some(p)=>Node::pre_order(&p),
+            None=>println!("The Tree is empty")
+        }
+    }
+    pub fn in_order(&self){
+        match &self.root{
+            Some(p)=>Node::in_order(&p),
+            None=>println!("The Tree is empty")
+        }
+    }
+    pub fn post_order(&self){
+        match &self.root{
+            Some(p)=>Node::post_order(&p),
+            None=>println!("The Tree is empty")
+        }
+    }
+    pub fn insert_node(&mut self,val:String){
+        match &mut self.root{
+            Some(p)=>{Node::insert_node(p,val)},
+            None=>{
+                let node=Node::create_node(val);
+                let pointer=Some(Box::new(node));
+                self.root=pointer;
+            }
+        }
+    }
+}
