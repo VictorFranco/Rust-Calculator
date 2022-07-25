6875117b9458ece638c1fd4075631505c64852cd
Victor Franco <vfrancohdez1@hotmail.com>
2022-07-25 14:46:54 -0500

    feat: add support for numbers with multiple signs

--- src/calculator.rs
+++ src/calculator.rs
@@ -18,20 +18,20 @@ pub fn compute(instructions: &[&str]) -> Result<f64, &'static str> {
             Stack::pop(&mut stack, &mut num1).unwrap();
             Stack::pop(&mut stack, &mut num2).unwrap();
             let result = match expression {
-                "+" => num2+num1,
-                "-" => num2-num1,
-                "*" => num2*num1,
+                "+" => num2 + num1,
+                "-" => num2 - num1,
+                "*" => num2 * num1,
                 "/" => {
                     if num1 == 0.0 {
                         return Err("Error: Cannot divide by 0");
                     }
-                    num2/num1
+                    num2 / num1
                 },
                 _   => 0.0
             };
             Stack::push(&mut stack, result).unwrap();
         } else {
-            let number: f64 = expression.parse::<f64>().unwrap();
+            let number: f64 = parse(&expression);
             Stack::push(&mut stack, number).unwrap();
         }
     }
@@ -41,6 +41,23 @@ pub fn compute(instructions: &[&str]) -> Result<f64, &'static str> {
     } else {
         Ok(stack.array[0])
     }
+}
+
+pub fn parse(expression: &str) -> f64 {     // parse numbers with multiple signs
+    let mut sign = 1.0;
+    let mut start_number = 0;
+    for (index, character) in expression.chars().enumerate() {
+        match character {
+            '+' | ' ' => {},                // positive sign does not affect the number sign
+            '-' => {sign = sign * -1.0},    // change the number sign
+            _   => {                        // the character is a digit
+                start_number = index;       // save the number position
+                break;
+            }
+        }
+    }
+    let number: f64 = (&expression[start_number ..]).parse::<f64>().unwrap();
+    sign * number
 }
 
 #[allow(dead_code)]
@@ -57,23 +74,26 @@ pub fn show_math_expre(instructions: &[&str]) {
 pub fn get_math_array<'a>(instructions: &'a str) -> [&'a str; N] {
     let mut math_expre: [&'a str; N] = [""; N];
     let mut counter = 0;    // array index
-    let mut wait = 0;       // wait for digits
+    let mut wait = 0;       // wait for number
+    let mut operation = false;
     for (index, character) in instructions.chars().enumerate() {
-        match (character, wait) {
-            ('+' | '-', 0) => {             // positive or negative sign
+        match (character, operation) {
+            ('+' | '-', false) => {             // character is positive or negative sign
                 math_expre[counter] = &instructions[index - wait .. index + 1];
                 wait = wait + 1;
             },
-            ('+' | '-' | '*' | '/', _) => { // binary operations
+            ('+' | '-' | '*' | '/', true) => {  // character is a binary operation
                 counter = counter + 1;
                 math_expre[counter] = &instructions[index .. index + 1];
                 counter = counter + 1;
                 wait = 0;
+                operation = false;          // the next character is a sign or a digit
             },
-            (' ', _) => {},                 // ignore whitespaces
-            _   => {                        // add a digit
+            (' ', _) => {wait = wait + 1},  // ignore whitespaces
+            _   => {                        // character is a digit
                 math_expre[counter] = &instructions[index - wait .. index + 1];
-                wait = wait + 1;
+                wait  = wait + 1;
+                operation = true;           // the next character is an operation or a digit
             }
         }
     }
